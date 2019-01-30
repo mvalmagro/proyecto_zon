219641a41ae046316403681c611275b3c9eac5c0
jezZu13 <jezzu1311@gmail.com>
2019-01-31 00:42:03 +0100

    Estoy empezando a diseñar la pantalla del cliente

---
+++ Proyecto_Final_GrupoZon/ventana_proyectos.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="ventana_proyectos.aspx.vb" Inherits="Proyecto_Final_GrupoZon.ventana_proyectos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+        <link href="Content/materialize.min.css" rel="stylesheet" />
+    <link href="Content/esitlos_ventana_proyectos.css" rel="stylesheet" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" class="form-container">
+        <div class="container">
+            <div class="row">
+                    <div class="col-6">
+                        <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+                    </div>
+                    <div class="col-6">
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+
+                    </div>
+                    
+                </div>
+            
+            <div class="row">
+                <div class="col-6">
+                    <asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+                </div>
+           </div>
+      </div>
+
+    </form>
+    <script src="Scripts/jquery-3.3.1.min.js"></script>
+            <script src="Scripts/bootstrap.min.js"></script>
+        <script src="Content/materialize.min.js"></script>
+
+
+
+
+</body>
+</html>
---
+++ Proyecto_Final_GrupoZon/ventana_proyectos.aspx.designer.vb
@@ -0,0 +1,51 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class ventana_proyectos
+    
+    '''<summary>
+    '''Control form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+    
+    '''<summary>
+    '''Control Calendar1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Calendar1 As Global.System.Web.UI.WebControls.Calendar
+    
+    '''<summary>
+    '''Control TextBox1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TextBox1 As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control Calendar2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Calendar2 As Global.System.Web.UI.WebControls.Calendar
+End Class
