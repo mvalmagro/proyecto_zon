8aa2a96018016bb6360bed1d2766d55e21bb92c0
mvalmagro <mavialmagro_06@hotmail.com>
2019-02-03 18:19:00 +0100

    Programacion de formulario de alta de usuarios.

---
+++ Proyecto_Final_GrupoZon/Alta_usuario.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Alta_usuario.aspx.vb" Inherits="Proyecto_Final_GrupoZon.Alta_usuario" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" />
+
+        <script type ="text/javascript">
+            function visible(boton) {
+
+                if (boton == "img1") {
+                    document.getElementById('<%= txtPassword.ClientID%>').setAttribute("type", "text");
+                }
+                else {
+                    document.getElementById('<%= txtPasswordConfirm.ClientID%>').setAttribute("type", "text");
+                }
+                
+            }  
+
+            function oculto(boton) {
+                if (boton == "img1") {
+                    document.getElementById('<%= txtPassword.ClientID%>').setAttribute("type", "password");
+                }
+                else {
+                    document.getElementById('<%= txtPasswordConfirm.ClientID%>').setAttribute("type", "password");
+                }
+                
+            }  
+            
+
+    </script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Alta usuario</h1>
+
+            <br />
+
+            <asp:Label ID="Label1" runat="server" Text="Label">Usuario:</asp:Label>
+            <asp:TextBox ID="txtUsuario" runat="server"></asp:TextBox>
+            
+            <br />
+
+            <asp:Label ID="Label2" runat="server" Text="Label">Password:</asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+            <i class="fa fa-eye" onmousedown="visible('img1')" onmouseup="oculto('img1')"></i>
+           
+            <br />
+
+            <asp:Label ID="Label3" runat="server" Text="Label">Confirmar password:</asp:Label>
+            <asp:TextBox ID="txtPasswordConfirm" runat="server" TextMode="Password"></asp:TextBox>
+            <i class="fa fa-eye" onmousedown="visible('img2')" onmouseup="oculto('img2')"></i>
+            
+            <br />
+
+            <asp:Label ID="Label4" runat="server" Text="Label">Nombre y apellidos:</asp:Label>
+            <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+            
+            <br />
+
+            <asp:Label ID="Label5" runat="server" Text="Label">Email:</asp:Label>
+            <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            
+            <br />
+
+            <asp:DropDownList ID="dropDownRol" runat="server">
+  
+            </asp:DropDownList>
+
+            <br />
+
+            <asp:Button ID="bttAlta" runat="server" Text="Dar de alta" />
+
+        </div>
+        
+    </form>
+    
+</body>
+
+
+</html>
---
+++ Proyecto_Final_GrupoZon/Alta_usuario.aspx.vb
@@ -0,0 +1,164 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Alta_usuario
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        CargarRoles()
+
+    End Sub
+
+    Private Sub bttAlta_Click(sender As Object, e As EventArgs) Handles bttAlta.Click
+        Dim usuario As String
+        Dim password1 As String
+        Dim password2 As String
+        Dim nombreyapellidos As String
+        Dim email As String
+        Dim rol As String
+
+        usuario = txtUsuario.Text
+        password1 = txtPassword.Text
+        password2 = txtPasswordConfirm.Text
+        nombreyapellidos = txtNombre.Text
+        email = txtEmail.Text
+        rol = dropDownRol.SelectedValue
+
+
+        If usuario.Equals("") = True Or password1.Equals("") = True Or password2.Equals("") = True Or nombreyapellidos.Equals("") = True Then
+
+            MsgBox("Debe rellenar los campos obligatorios", MsgBoxStyle.Exclamation, "Proceso de alta de usuario fallido.")
+            'Response.Redirect("Alta_usuario.aspx") '-->Recargamos la página
+
+        Else
+            'Comprobar que el nombre de usuario a dar de alta no existe:
+            If ComprobarDuplicidadUsuario(usuario) = True Then
+                MsgBox("El usuario introducido ya existe.", MsgBoxStyle.Exclamation, "Proceso de alta de usuario fallido.")
+                'Response.Redirect("Alta_usuario.aspx") '-->Recargamos la página
+
+            Else
+                'Comprobar que existe coincidencia de contraseña:
+                If password1.Equals(password2) = False Then
+                    MsgBox("Las contraseñas introducidas no coinciden.", MsgBoxStyle.Exclamation, "Proceso de alta de usuario fallido.")
+                    'Response.Redirect("Alta_usuario.aspx") '-->Recargamos la página
+
+                End If
+
+                'Comprobar la seguridad de contraseña
+
+                'Ejecutar query para el alta
+
+            End If
+
+
+
+
+
+
+        End If
+
+
+    End Sub
+
+
+#Region "Funciones"
+
+    Private Sub CargarRoles()
+        'Realizamos la conexión a la BBDD:
+
+        Dim cadenaConexion As String = "Server=pmssql100.dns-servicio.com;Database=6438944_zon;User Id=jrcmvaa;Password=Ssaleoo9102;"
+        Dim oConexion As New SqlConnection
+        Dim myCmd As SqlCommand
+        Dim oDataAdapter As SqlDataAdapter
+        Dim oDataSet As DataSet
+        Dim i As Integer
+        Dim oDataRow As DataRow
+        Dim lstItem As ListItem
+
+        oConexion = New SqlConnection(cadenaConexion)
+
+        oConexion.Open() '-->abrimos la conexión
+
+        myCmd = New SqlCommand("SELECT id, nombre FROM roles", oConexion)
+
+        oDataAdapter = New SqlDataAdapter(myCmd)
+
+        oDataSet = New DataSet
+        oDataAdapter.Fill(oDataSet, "roles")
+
+        oDataAdapter = Nothing
+
+        oConexion.Close()
+
+        'dropDownRol.Items.Clear()
+
+        i = 0
+        While i < oDataSet.Tables("roles").Rows.Count
+            oDataRow = oDataSet.Tables("roles").Rows(i)
+
+            lstItem = New ListItem(oDataRow("nombre"), oDataRow("id"))
+
+            dropDownRol.Items.Insert(i, lstItem)
+
+            lstItem = Nothing
+            oDataRow = Nothing
+            i = i + 1
+        End While
+
+        oDataSet = Nothing
+
+    End Sub
+
+
+    Public Function ComprobarDuplicidadUsuario(l_usuario) As Boolean
+        Dim cadenaConexion As String = "Server=pmssql100.dns-servicio.com;Database=6438944_zon;User Id=jrcmvaa;Password=Ssaleoo9102;"
+        Dim oConexion As New SqlConnection
+        Dim myCmd As SqlCommand
+        Dim oDataAdapter As SqlDataAdapter
+        Dim oDataSet As DataSet
+        Dim i As Integer
+        Dim oDataRow As DataRow
+
+
+        oConexion = New SqlConnection(cadenaConexion)
+
+        oConexion.Open()
+
+        myCmd = New SqlCommand("SELECT nombre FROM usuarios", oConexion)
+
+        oDataAdapter = New SqlDataAdapter(myCmd)
+
+        oDataSet = New DataSet
+        oDataAdapter.Fill(oDataSet, "usuarios")
+
+        oDataAdapter = Nothing
+
+        oConexion.Close()
+
+        i = 0
+        While i < oDataSet.Tables("usuarios").Rows.Count
+            oDataRow = oDataSet.Tables("usuarios").Rows(i)
+
+            If oDataRow("nombre").Equals(l_usuario) = True Then
+                Return True '-->Usuario duplicado y cortamos aquí la ejecución de esta función.
+            End If
+
+            oDataRow = Nothing
+            i = i + 1
+        End While
+
+
+        Return False
+
+
+    End Function
+
+
+
+
+#End Region
+
+
+
+
+
+End Class
