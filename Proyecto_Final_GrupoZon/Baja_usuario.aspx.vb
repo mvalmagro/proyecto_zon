a9de737bfa31d7df45d244c7f0b86cc304cc92b7
mvalmagro <mavialmagro_06@hotmail.com>
2019-02-04 08:39:31 +0100

    Retocamos el codigo de baja de usuario.

--- Proyecto_Final_GrupoZon/Baja_usuario.aspx.vb
+++ Proyecto_Final_GrupoZon/Baja_usuario.aspx.vb
@@ -10,11 +10,83 @@ Public Class Baja_usuario
 
     End Sub
 
-    Private Sub Baja_usuario_LoadComplete(sender As Object, e As EventArgs) Handles Me.LoadComplete
+    Private Sub grdviewUsuarios_RowDataBound(sender As Object, e As GridViewRowEventArgs) Handles grdviewUsuarios.RowDataBound
+        'Ocultamos la primera columna del Grid (ID):
+        e.Row.Cells(1).Visible = False
+
+        'Ocultamos la última columna del Grid (Fecha de ultimo logon):
+        e.Row.Cells(6).Visible = False
+    End Sub
+
+    Private Sub grdviewUsuarios_RowDeleting(sender As Object, e As GridViewDeleteEventArgs) Handles grdviewUsuarios.RowDeleting
+        Dim idUsuario As String
+        Dim nombreUsuario As String
+        Dim fechaLastLogon As Date
+
+        idUsuario = grdviewUsuarios.Rows(e.RowIndex).Cells(1).Text
+        nombreUsuario = grdviewUsuarios.Rows(e.RowIndex).Cells(2).Text
+        fechaLastLogon = grdviewUsuarios.Rows(e.RowIndex).Cells(6).Text
+
+        'If Session("usuario").Equals(nombreUsuario) = False Then
+
+        If ComprobarUltimoLogonUsuario(fechaLastLogon) = True Then
+
+                If MsgBox("¿Estás seguro que desea eliminar al usuario " & nombreUsuario & "?", MsgBoxStyle.OkCancel, "Eliminación de usuario") = MsgBoxResult.Ok Then
+                    'Se lanza la eliminación del usuario en la tabla de "privilegios_usuarios":
+                    Dim cadenaConexion As String = "Server=pmssql100.dns-servicio.com;Database=6438944_zon;User Id=jrcmvaa;Password=Ssaleoo9102;"
+                    Dim oConexion As New SqlConnection
+                    Dim myCmd As SqlCommand
+
+                    oConexion = New SqlConnection(cadenaConexion)
+
+                    oConexion.Open()
+
+                    myCmd = New SqlCommand("DELETE FROM privilegios_usuarios WHERE id_usuario=" & idUsuario, oConexion)
+                    myCmd.ExecuteNonQuery()
+                    myCmd = Nothing
+
+                    myCmd = New SqlCommand("DELETE FROM usuarios WHERE id=" & idUsuario, oConexion)
+                    myCmd.ExecuteNonQuery()
+                    myCmd = Nothing
+
+                    oConexion.Close()
+
+                    Response.Redirect("Baja_usuario.aspx") '-->Recargamos la página
+
+
+                Else
+                    e.Cancel = True
+                End If
+
+            Else
+                MsgBox("No se puede eliminar al usuario " & nombreUsuario & " porque inició sesión hace menos de dos días.", MsgBoxStyle.Exclamation, "Eliminación de usuario")
+                e.Cancel = True
+
+            End If
+
+        'Else
+        'MsgBox("No se puede eliminar al usuario " & nombreUsuario & " porque se trata del usuario de la sesión actual.", MsgBoxStyle.Exclamation, "Eliminación de usuario")
+        'e.Cancel = True
+        ' End If
 
     End Sub
 
 #Region "Funciones"
+
+    Private Function ComprobarUltimoLogonUsuario(l_fechaLastLogon As Date) As Boolean
+        Dim fechaActual As Date
+        Dim i As Integer
+
+        fechaActual = Today
+
+        'Sumamos dos días a la fecha de  ultimo login del usuario  y la comparamos con la fecha actual:
+        If l_fechaLastLogon.AddDays(2).CompareTo(fechaActual) <= 0 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
 
     Private Sub CargarDatos()
 
@@ -28,7 +100,7 @@ Public Class Baja_usuario
 
         oConexion.Open()
 
-        myCmd = New SqlCommand("SELECT u.id, u.nombre AS 'usuario', u.nombre_real AS 'nombre y apellidos', u.email, r.nombre AS 'rol' FROM usuarios u, roles r WHERE u.id_rol=r.id", oConexion)
+        myCmd = New SqlCommand("SELECT u.id, u.nombre AS 'Usuario', u.nombre_real AS 'Nombre', u.email AS 'Correo electronico', r.nombre AS 'Rol', ultimo_logon FROM usuarios u, roles r WHERE u.id_rol=r.id", oConexion)
 
         oSqlDataReader = myCmd.ExecuteReader
         oDataTable.Load(oSqlDataReader)
@@ -38,47 +110,6 @@ Public Class Baja_usuario
         grdviewUsuarios.DataBind()
 
         oConexion.Close()
-
-    End Sub
-
-    Private Sub grdviewUsuarios_RowDataBound(sender As Object, e As GridViewRowEventArgs) Handles grdviewUsuarios.RowDataBound
-        'Ocultamos la primera columna del Grid:
-        e.Row.Cells(1).Visible = False
-    End Sub
-
-    Private Sub grdviewUsuarios_RowDeleting(sender As Object, e As GridViewDeleteEventArgs) Handles grdviewUsuarios.RowDeleting
-        Dim idUsuario As String
-        Dim nombreUsuario As String
-
-        idUsuario = grdviewUsuarios.Rows(e.RowIndex).Cells(1).Text
-        nombreUsuario = grdviewUsuarios.Rows(e.RowIndex).Cells(2).Text
-
-        If MsgBox("¿Estás seguro que desea eliminar al usuario " & nombreUsuario, MsgBoxStyle.OkCancel, "Eliminación de usuario") = MsgBoxResult.Ok Then
-            'Se lanza la eliminación del usuario en la tabla de "privilegios_usuarios":
-            Dim cadenaConexion As String = "Server=pmssql100.dns-servicio.com;Database=6438944_zon;User Id=jrcmvaa;Password=Ssaleoo9102;"
-            Dim oConexion As New SqlConnection
-            Dim myCmd As SqlCommand
-
-            oConexion = New SqlConnection(cadenaConexion)
-
-            oConexion.Open()
-
-            myCmd = New SqlCommand("DELETE FROM privilegios_usuarios WHERE id_usuario=" & idUsuario, oConexion)
-            myCmd.ExecuteNonQuery()
-            myCmd = Nothing
-
-            myCmd = New SqlCommand("DELETE FROM usuarios WHERE id=" & idUsuario, oConexion)
-            myCmd.ExecuteNonQuery()
-            myCmd = Nothing
-
-            oConexion.Close()
-
-            Response.Redirect("Baja_usuario.aspx") '-->Recargamos la página
-
-
-        Else
-            e.Cancel = True
-        End If
 
     End Sub
 
